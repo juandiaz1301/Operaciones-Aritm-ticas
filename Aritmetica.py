00a512b65d40b68a0f8095085a6fad0620aaa789
juandiaz1301 <juandiaz@colegioguanenta.edu.co>
2024-02-22 17:17:54 -0500

    se realizo analisis y diseño

---
+++ Aritmetica.py
@@ -0,0 +1,26 @@
+# programa para calcular la suma, resta, multiplicacion, division, division entera, modulo y potencia de 2 unidades
+
+# inpunt
+x= int (input("Por favor ingrese el numero a calcular:"))
+y= int (input("por favor ingrese el numero a calcular:"))
+
+# processing
+
+s= (x+y)
+r= (x-y)
+m= (x*y)
+d= (x/y)
+de= (x//y)
+mod= (x%y)
+p= (x**y)
+
+# output
+
+print ("Este es el resultado de los numeros dados para las operaciones:")
+print ("Suma:",s)
+print ("Resta:",r)
+print ("Multiplacion:",m)
+print ("Division:",d)
+print ("Division entera:",de)
+print ("Modulo:".mod)
+print ("Potencia:",p)
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# Operaciones-Aritm-ticas
+# Operaciones-Aritm-eticas
+Programa para calcular la suma, resta, multiplicacion, division, division entera, modulo y potencia de 2 unidades 
+
+## Calcular mediante 2 numeros enteros distintas operaciones.
+
+# Analisis 
+
+-Variable de entrada
+
+X y Y: Es igual a dos numeros naturales, para desarrollar las operaciones aritmeticas correspondientes. 
+
+-Variable de salida 
+
+s: x+y
+r: x-y
+m: x*y
+d: x/y
+de: x//y
+mod: x%y
+p: x**y
+
+
+# Diseño
+![Diagrama de flujo](diagrama.png "diagrama de flujo")
+
+# Construccion
